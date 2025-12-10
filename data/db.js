82ef99e15c4f97dac0e946a6865cdebff93cd05a
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-12-10 12:19:58 +0100

    Connect the server to the database

---
+++ data/db.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+	host: process.env.HOST,
+	user: process.env.DB_USER,
+	password: process.env.PASSWORD,
+	database: process.env.DATABASE,
+});
+
+connection.connect((err) => {
+	if (err) throw err;
+
+	console.log("Successful connection to DB!");
+});
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const connection = require("./data/db");
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+// Static files
+app.use(express.static("public"));
+// Body Parser
+app.use(express.json());
+
+app.listen(PORT, () => {
+	console.log(`Listening on http://localhost:${PORT}`);
+});
+
+// Main Route
+app.get("/", (req, res) => {
+	res.send("Main Index Route");
+});
